e76316104c7e5fc3b895db5eb52e615dba0d3a42
Nasser Boukehil <boukehil.nasreddine@gmail.com>
2023-04-26 22:34:03 +0200

    added interfaces in data

---
+++ pet-clinic-data/src/main/java/com/nasdev/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.nasdev.petclinic.services;
+
+import com.nasdev.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/nasdev/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.nasdev.petclinic.services;
+
+import com.nasdev.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+}
