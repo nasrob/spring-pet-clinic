176139b7d38449eec55ae76e5c5ce146bafbc3de
Nasser Boukehil <boukehil.nasreddine@gmail.com>
2023-05-02 04:02:20 +0200

    refactored crud service with interface

--- pet-clinic-data/src/main/java/com/nasdev/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/nasdev/petclinic/services/VetService.java
@@ -2,13 +2,5 @@ package com.nasdev.petclinic.services;
 
 import com.nasdev.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
