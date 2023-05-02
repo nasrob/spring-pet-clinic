292008cc8d4d82c9f9c8e43f6da7b1dec0d3f6d4
Nasser Boukehil <boukehil.nasreddine@gmail.com>
2023-05-02 04:25:38 +0200

    implemented crud service within map based services

---
+++ pet-clinic-data/src/main/java/com/nasdev/petclinic/services/map/PetService.java
@@ -0,0 +1,34 @@
+package com.nasdev.petclinic.services.map;
+
+import com.nasdev.petclinic.model.Pet;
+import com.nasdev.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/nasdev/petclinic/services/map/VetService.java
@@ -0,0 +1,34 @@
+package com.nasdev.petclinic.services.map;
+
+import com.nasdev.petclinic.model.Vet;
+import com.nasdev.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+}
